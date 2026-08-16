1d4faa74aea9598274f33a901438d64a43157c2d
David Gómez Sanabria <david.gs@globant.com>
2026-08-16 12:09:03 +0200

    chore: configure vote storage endpoint

--- vote-config.js
+++ vote-config.js
@@ -1,2 +1,2 @@
 // Paste the deployed Google Apps Script Web App URL between the quotes.
-window.XP_VOTE_ENDPOINT = 'PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE';
+window.XP_VOTE_ENDPOINT = 'https://script.google.com/macros/library/d/1gk_FEozCQ93nOHQhqUbA5EvxG94f5rkWaidTsbHX78Ba3T9SzuZlIaZC/1';
